b2f5384eb6cfcf0c87fe5bcce56962ee773e623d
V J <sunilkumara.v.j@accenture.com>
2020-02-20 09:50:44 +0530

    Updates

---
+++ precache-manifest.ed94c389168cb1cc47194cb446a5fa41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51e5e06d23f136945a3de1a71a73a008",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "163f7ab5017f826c7da6",
+    "url": "/my-app/static/css/main.905bdffc.chunk.css"
+  },
+  {
+    "revision": "928044dc99f6a9d75482",
+    "url": "/my-app/static/js/2.d5a6855d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-app/static/js/2.d5a6855d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "163f7ab5017f826c7da6",
+    "url": "/my-app/static/js/main.8ca9e67f.chunk.js"
+  },
+  {
+    "revision": "134c6d7b033e4b816dc9",
+    "url": "/my-app/static/js/runtime-main.56c001b7.js"
+  }
+]);
